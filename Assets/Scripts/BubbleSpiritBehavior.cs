09d2f450e8b8c17dd919705cc8763506bb423053
y <12588017+y-ack@users.noreply.github.com>
2020-11-13 19:24:52 +0000

    statistics singleton

--- Assets/Scripts/BubbleSpiritBehavior.cs
+++ Assets/Scripts/BubbleSpiritBehavior.cs
@@ -11,7 +11,7 @@ public class BubbleSpiritBehavior : MonoBehaviour
         yellow,
         rainbow
     }
-    
+
     public GameObject testingInitialParent; //don't use this for production spawning
     private GridLayout parentGrid; // this one is ok to keep, since it's just cached
     public BubbleColor color;
@@ -21,22 +21,77 @@ public class BubbleSpiritBehavior : MonoBehaviour
 
     void Start()
     {
+        // only for testing, instead set transform.parent to transform of
+        // whatever bubble unit it's being added to at spawn
         transform.parent = testingInitialParent.transform;
+
+        // should this be in start or moved to a separate UpdateParent()?
         parentGrid = transform.parent.GetComponent<GridLayout>();
-        SnapToGrid();
+        UpdateGridPosition();
     }
 
     void Update()
     {
-        
+        // should probably stay empty? when will bubbles move without parent?
+        // maybe when captured?
+        // TODO[BETA] add gleam animation for bubble spirits
     }
 
-    private void SnapToGrid()
+    // called once grid position is determined.
+
+    private void UpdateGridPosition()
     {
         transform.localPosition = parentGrid.CellToLocal(gridPosition);
     }
+
+    // used when an independent bubble collides and is added to a bubbleunit
+    // e.g. before capture chaining to determine the closest grid placement
+    // TODO[BETA]: lerp to new localposition for smoother snap?
+    private void SnapToGrid()
+    {
+        gridPosition = parentGrid.WorldToCell(transform.position);
+        UpdateGridPosition();
+    }
+
     private void AdoptedBy(/*GameObject newParentUnit*/)
     {
         /* for combining/launching captured bubbles to set a new parent bubbleunit*/
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //a) player bullet: set cleared flag, unset parent transform
+        //                  trigger animation, delete/return to pool
+
+        // handled by parent unit?: also maybe TODO[ALPHA]?
+        //b) wall: cannot pass through, bounces off
+        //c) flora: bounces off rocks, passes through bushes
+        //d) others: ignored
+    }
+
+    void Clear()
+    {
+        cleared = true;
+        RunStatistics.Instance.bubblesCleared++;
+        transform.parent = null;
+        //trigger animation, yield and delete
+    }
+    void ChainClear()
+    {
+        // recursive border case
+        if (cleared)
+        {
+            return;
+        }
+        
+        Clear();
+
+        // TODO[DEMO] check neighbors, call ChainClear if color matches
+        //            currently don't have a way to check neighbors.
+        //            store and query the parent BubbleUnit array?
+        // TODO[ALPHA] if color is a struct with a match method, can move
+        //             responsibility for doing match check to color type
+        
+        
     }
 }
---
+++ Assets/Scripts/RunStatistics.cs
@@ -0,0 +1,35 @@
+﻿// the statistics data class.
+// access it with e.g.
+// RunStatistics.Instance.bubblesCleared++
+//
+public class RunStatistics : Singleton<RunStatistics>
+{
+    // (Optional) Prevent non-singleton constructor use.
+    protected RunStatistics() { }
+
+    // these are stats for a single run, not sure if totals should
+    // be separate or also accessed here
+
+    public string playerName;
+    //don't set this, save manager will calculate at run finish
+    public float time;
+    public int controlMode;
+    public int stagesCleared;
+    public int totalScore;
+    public int bubblesCleared;
+
+    public int bubblesChainClearedRed;
+    public int bubblesChainClearedBlue;
+    public int bubblesChainClearedYellow;
+    public int bubblesChainClearedOther;
+    // Total Chain Clears: calculate when displaying
+    // Clears Per Minute: calculate when displaying
+
+    public int trapsUsed;
+    public int capturesUsed;
+    public int trapsMissed; //set when player bullet hits wall/expires
+
+    public float grazeTime;
+    public float focusTime;
+    public int rollsPerformed;
+}
