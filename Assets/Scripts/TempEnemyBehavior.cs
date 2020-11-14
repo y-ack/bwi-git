01e632ac85ff30ca91e8b8968a95f9e32dc85bda
Sonlam Nguyen <37813537+SSonlam@users.noreply.github.com>
2020-11-14 05:09:21 +0000

    just for nathan, incomplete code

--- Assets/Scripts/TempEnemyBehavior.cs
+++ Assets/Scripts/TempEnemyBehavior.cs
@@ -4,23 +4,56 @@ using UnityEngine;
 
 public class TempEnemyBehavior : MonoBehaviour
 {
+    private float latestDirectionChangeTime;
+    private readonly float directionChangeTime = 3f;
+    private float characterVelocity = 2f;
+    private Vector2 movementDirection;
+    private Vector2 movementPerSecond;
     // Start is called before the first frame update
     void Start()
     {
-        
+        latestDirectionChangeTime = 0f;
+        calcuateNewMovementVector();
+    }
+    void calcuateNewMovementVector(){
+        //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
+        movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+        movementPerSecond = movementDirection * characterVelocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if the changeTime was reached, calculate a new movement vector
+        StartCoroutine(BubbleWander());
     }
+    IEnumerator BubbleWander()
+    {
+        yield return StartCoroutine(Wait());
+    }
+    IEnumerator Wait()
+    {
+        if (Time.time - latestDirectionChangeTime > directionChangeTime){
+            latestDirectionChangeTime = Time.time;
+            calcuateNewMovementVector();
+        }
+        transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime), 
+        transform.position.y + (movementPerSecond.y * Time.deltaTime));
+        yield return new WaitForSeconds(2);
+    }
+
+
 
     private void OnTriggerEnter2D(Collider2D collision){
         //taken mostly verbatim from class collision example
         if (collision.gameObject.tag == "Bullet" || collision.gameObject.tag == "Capture")
         {
             destroySelf();
+        }
+        if (collision.gameObject.tag == "Wall Top")
+        {
+            Debug.Log("Colliding wall");
+            calcuateNewMovementVector();
         }
         
     }
